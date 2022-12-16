dfdc88a29f00b52a655314fceeff3166a96ad063
Josh Hager <jhager603@gmail.com>
2022-12-16 18:44:23 -0500

    Cleaned up code, added comments

--- keystroke_backup.py
+++ keystroke_backup.py
@@ -1,6 +1,5 @@
 from time import sleep
 from pynput import keyboard
-from datetime import datetime
 import keyboard as kboard
 
 keylog = []
@@ -13,31 +12,42 @@ def log():
     global line_position
 
     with open('log.txt', 'a') as log:
-        log.write(''.join(keylog))
+        log.write(''.join(keylog)) # write the keylog to the log file
         line_position += len(keylog)
 
-        # new line if we're past 70 chars after the append
-        if line_position >= 70:
+        # new line if we're past 100 chars after the append
+        if line_position >= 100:
             log.write('\n')
             line_position = 0
 
-def on_press(key):
+def suggest():
+    global current_word
+    global words
+
+    if current_word in words:
+        print('Suggestions: ' + str(words[current_word]))
+    else:
+        print('No current suggestions.')
+
+
+def on_release(key):
     global current_word
     global prev_word
     global words
 
+    # try statement handles non-special keys (letters, punctuation, numbers, etc.)
     try:
-        # print(str(key.char))
-
-        if str(key.char) != 'None':
+        if str(key.char) != 'None': # hotkey registers as 'None'
             keylog.append(str(key.char))
 
             if str(key.char).isalpha():
                 current_word += str(key.char)
+    # except statement handles special keys(<esc>, <enter>, etc.)
     except AttributeError:
+
+        # space indicates user has finished typing a word
         if(str(key)[4:] == 'space'):
             keylog.append(' ')
-            # print(' ')
 
             if prev_word not in words:
                 words[prev_word] = [current_word, '', '']
@@ -45,78 +55,61 @@ def on_press(key):
                 words[prev_word].insert(0, current_word)
                 words[prev_word].pop()
 
-            print('Current word: ' + current_word)
-            print('Word pair: ' + prev_word + ' ' + current_word)
-
-            if current_word in words:
-                print('Current pairings: ' + str(words[current_word]))
-            else:
-                print('No current pairings.')
+            suggest()
 
             prev_word = current_word
             current_word = ''
         else:
             keylog.append('<' + str(key)[4:] + '>')
-            # print('<' + str(key)[4:] + '>')
-        
-listener = keyboard.Listener(on_press=on_press)
+
+listener = keyboard.Listener(on_release=on_release)
 listener.start()
 
+# take suggestion 1
 def on_activate_1():
     global words
     global prev_word
     global current_word
-    print('<ctrl>+<alt>+1 pressed')
 
     if prev_word in words:
         kboard.write(words[prev_word][0] + ' ')
+        suggest()
         prev_word = current_word
         current_word = ''
-    else:
-        print('No word in 1 spot')
-        print(prev_word)
-
+    
+# take suggestion 2
 def on_activate_2():
     global words
     global prev_word
     global current_word
-    print('<ctrl>+<alt>+2 pressed')
 
     if prev_word in words and words[prev_word][1] != '':
         kboard.write(words[prev_word][1] + ' ')
+        suggest()
         prev_word = current_word
         current_word = ''
-    else:
-        print('No word in 2 spot')
-        print(prev_word)
 
+# take suggestion 3
 def on_activate_3():
     global words
     global prev_word
     global current_word
-    print('<ctrl>+<alt>+3 pressed')
 
     if prev_word in words and words[prev_word][2] != '':
         kboard.write(words[prev_word][2] + ' ')
+        suggest()
         prev_word = current_word
         current_word = ''
-    else:
-        print('No word in 3 spot')
-        print(prev_word)
 
+# set up hotkeys
 hotkeys = keyboard.GlobalHotKeys({
          '<ctrl>+<alt>+1': on_activate_1,
          '<ctrl>+<alt>+2': on_activate_2,
          '<ctrl>+<alt>+3': on_activate_3})
 hotkeys.start()
 
-
-while '<esc>' not in keylog:
+# log every half second
+while True:
     log()
     keylog = []
-    # print('keylog cleared')
-    # print('collecting for 10 seconds')
-    sleep(10)
-
-
-    
+    sleep(.5)
